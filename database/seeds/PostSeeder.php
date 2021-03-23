1895dd2a387970d0f3b79544acf7ca3382b61f29
Marco.seb <marco.seba96@gmail.com>
2021-03-23 17:23:24 +0100

    creation of the posts table and insertion of fake data

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +12,14 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(4);
+            $newPost->content = $faker->text(500);
+            $newPost->slug = Str::slug($newPost->title);
+            $newPost->save();
+        }
     }
 }
