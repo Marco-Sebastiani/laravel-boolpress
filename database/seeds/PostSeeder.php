eeeb38694e79f7cd6dd4825cb528fbbcd856293e
Marco.seb <marco.seba96@gmail.com>
2021-03-26 01:24:19 +0100

    add unique slug

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Post;
+use App\User;
 use Illuminate\Support\Str;
 
 class PostSeeder extends Seeder
@@ -16,9 +17,25 @@ class PostSeeder extends Seeder
     {
         for($i=0; $i<10; $i++){
             $newPost = new Post();
-            $newPost->title = $faker->sentence(4);
+            $userCount = count(User::all()->toArray());
+            $newPost->user_id = rand(1,$userCount);
+
+
+            $newPost->title = $faker->sentence(3);
             $newPost->content = $faker->text(500);
-            $newPost->slug = Str::slug($newPost->title);
+            //QUI GESTISCO L'UNICITA' DELLO SLUG
+            $slug = Str::slug($newPost->title);
+            $initialSlug = $slug;
+            $presentPost=Post::where('slug',$slug)->first();
+            $counter = 1;
+
+            while($presentPost){
+                $slug = $initialSlug . '-' . $counter;
+                $presentPost=Post::where('slug',$slug)->first();
+                $counter++;
+            }
+
+            $newPost->slug =  $slug;
             $newPost->save();
         }
     }
